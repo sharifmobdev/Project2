529358e3fcb366d45212decb1d85bcdbd1d1e645
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2019-04-15 12:16:11 +0430

    Add Volley library and implement basic request for getting posts

--- app/src/main/java/android/sharif/ir/courseproj2/MainActivity.java
+++ app/src/main/java/android/sharif/ir/courseproj2/MainActivity.java
@@ -4,7 +4,21 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +40,21 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        RequestQueue ExampleRequestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest ExampleRequest = new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+            }
+
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+                Log.i("Sharif", "onErrorResponse: error" + error.toString());
+            }
+        });
+        ExampleRequestQueue.add(ExampleRequest);
     }
 }
